8c1aee10e24846289c7680ffce186c55f37a11d7
greensmith <greensmith+github@gmail.com>
2015-07-09 15:56:52 +0100

    improve md3000 sets clock on desync

--- nagios/check_md3000.sh
+++ nagios/check_md3000.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 # check status of dell md3000 storage array
 # change /opt/dell/.... to folder containing SMcli
-# input from nagios is ip address of storage array
+# input from nagios is ip address of storage array controllers
 # checks with regex for status.
-
+# use two ips $1 and $2
 Client="/opt/dell/mdstoragemanager/client/SMcli"
 
 #run check status
-Status=$($Client $1 -S -c "show storageArray healthStatus;" -quick)
+Status=""
+Status1=$($Client $1 -S -c "show storageArray healthStatus;" -quick)
+Status2=$($Client $2 -S -c "show storageArray healthStatus;" -quick)
 
 
-ErrCode=2
+ErrCode=0
 
-if [[ $Status =~ "synchronization" ]]
+if [[ $Status1 =~ "synchronization" ]]
  then
-  ErrCode=1
+  #run sync
+  $Client $1 -S -c "set storageArray time;" -quick
+  Status1=$($Client $1 -S -c "show storageArray healthStatus;" -quick)
 fi
 
-if [[ $Status =~ "optimal" ]]
+if [[ $Status2 =~ "synchronization" ]]
  then
-  ErrCode=0
+  #run sync
+  $Client $2 -S -c "set storageArray time;" -quick
+  Status2=$($Client $2 -S -c "show storageArray healthStatus;" -quick)
 fi
+
+
+if [[ ! $Status1 =~ "optimal" ]] || [[ ! $Status2 =~ "optimal" ]]
+ then
+  ErrCode=2
+  Status="$Status1 $Status2"
+else
+ Status="$Status1"
+fi
+
+
 
 case $ErrCode in
  0) echo $Status
